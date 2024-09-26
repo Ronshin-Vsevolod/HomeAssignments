da3515437545c429dd488ab8cadecb7324ae0ad1
Ronshin-Vsevolod <st132572@student.spbu.ru>
2024-09-26 23:38:02 +0300

    Добавлен Hello.h с реализацией include guards.

---
+++ HelloWord.cpp
@@ -0,0 +1,11 @@
+/* Ronshin Vsevolod st132572@student.spbu.ru
+    first project
+*/
+#include <iostream>
+
+void SayHello(std::string word)
+    {
+    std::cout<<"Hello, "<<word<<std::endl;
+    }
+
+
