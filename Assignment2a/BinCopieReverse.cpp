ff14fde39b60a44d4f9b844fc1d702386d19bb76
Ronshin-Vsevolod <st132572@student.spbu.ru>
2024-10-02 23:54:34 +0300

    The assignment2a has been done

--- Assignment2a/BinCopieReverse.cpp
+++ Assignment2a/BinCopieReverse.cpp
@@ -1,6 +1,7 @@
 /* Ronshin Vsevolod st132572@student.spbu.ru
-Assignment in progress...
+This reads and copies inverted pdf files
 */
+
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -9,6 +10,31 @@ Assignment in progress...
 int main(int argc, char** argv)
 {
     std::ifstream infile;
-    infile.open("Data.pdf",std::ios::binary|std::ios::in);
-    std::cout<<"File size: "<<std::filesystem::file_size("/home/vsevolod-ronshin/HomeAssignments/Assignment2a/Data.pdf")<<std::endl;
+    infile.open("source.pdf",std::ios::binary|std::ios::in);
+    if (infile.is_open())
+    {
+        const int fsize = std::filesystem::file_size("/home/vsevolod-ronshin/HomeAssignments/Assignment2a/source.pdf");
+        char *buffer = new char[fsize];
+        std::ofstream outfile;
+        outfile.open("temppdf.pdf",std::ios::binary|std::ios::out);
+        // For some reason this code does not work for me without my own edits :(
+        //infile.read((char *)&buffer,sizeof(buffer));
+        //outfile.write((char *)&buffer,sizeof(buffer));
+        infile.read(buffer, fsize);
+        for (int i = 0; i < fsize / 2; i++) {
+            int temp = buffer[i];
+            buffer[i] = buffer[fsize - i - 1];
+            buffer[fsize - i - 1] = temp;
+        }
+        outfile.write(buffer, fsize);
+
+        delete [] buffer;
+        infile.close();
+        outfile.close();
+    }
+    else
+    {
+        std::cout<<"Error.The file cannot be opened"<<std::endl;
+    }
+    return 0;
 }
