239f644fceacc5f7fc1e239bce92dab600cd98e6
Ronshin-Vsevolod <st132572@student.spbu.ru>
2024-10-05 15:20:10 +0300

    The calculator is finalized. The calculator function (method) with auxiliary functions add and pop for adding and removing data from the stack is made directly.

--- Assignment2/b/Calculator.cpp
+++ Assignment2/b/Calculator.cpp
@@ -6,6 +6,7 @@ Calculator like “MK 61/54” (using *+/-)
 #include <fstream>
 #include <filesystem>
 #include <cstring>
+#include <sstream>
 
 class Calc
 {
@@ -13,10 +14,61 @@ public:
     int* mstack = new int[128];
     int top=0;
 
+    void add(int newnumb)
+    {
+        if(top<2)
+        {
+            mstack[top++] = newnumb;
+        }
+        else
+        {
+            int temp=pop();
+            pop();
+            add(temp);
+            add(newnumb);
+        }
+    }
+    int pop()
+    {
+        return mstack[--top];
+    }
+
     void calculate(char* data, int* result)
     {
-        //I'm still trying to write something workable here now
-        *result=11;
+        std::istringstream iss(data);
+        std::string token;
+
+        while (iss >> token)
+        {
+            if (isdigit(token[0]))
+            {
+                add(std::stoi(token));
+            }
+            else
+            {
+                int b = pop();
+                int a = pop();
+                switch (token[0])
+                {
+                    case '+':
+                        add(a + b);
+                        break;
+                    case '-':
+                        add(a - b);
+                        break;
+                    case '*':
+                        add(a * b);
+                        break;
+                    case '/':
+                        add(a / b);
+                        break;
+                    default:
+                        std::cout<<"Error: the program doesn't work with " << token << std::endl;
+                        break;
+                }
+            }
+        }
+        *result=pop();
         delete[] mstack;
     }
 };
@@ -32,10 +84,11 @@ int main() {
         source.close();
         int result;
         std::cout<<data<<std::endl;
+        data[fsize] = '\0';
 
         Calc calculator;
         calculator.calculate(data, &result);
-        std::cout << "Result: " << result << std::endl; //Expected result: 30
+        std::cout << "Result: " << result << std::endl;
 
         delete[] data;
     }
