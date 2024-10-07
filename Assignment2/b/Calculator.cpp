7080b5e0013c529a4aa7f1925d6b1576eda33c39
Ronshin-Vsevolod <st132572@student.spbu.ru>
2024-10-07 22:35:04 +0300

    Added the Calc.cpp file, where the Calc class has been moved, and Calculator.h

--- Assignment2/b/Calculator.cpp
+++ Assignment2/b/Calculator.cpp
@@ -5,74 +5,7 @@ Calculator like “MK 61/54” (using *+/-)
 #include <iostream>
 #include <fstream>
 #include <filesystem>
-#include <cstring>
-#include <sstream>
-
-class Calc
-{
-public:
-    double* mstack = new double[128];
-    int top=0;
-
-    void add(double newnumb)
-    {
-        if(top<2)
-        {
-            mstack[top++] = newnumb;
-        }
-        else
-        {
-            std::cerr<<"Error: Too many operands entered (max 2) \n";
-            double temp=pop();
-            pop();
-            add(temp);
-            add(newnumb);
-        }
-    }
-    double pop()
-    {
-        return mstack[--top];
-    }
-
-    void calculate(char* data, double* result)
-    {
-        std::istringstream iss(data);
-        std::string token;
-
-        while (iss >> token)
-        {
-            if (isdigit(token[0]))
-            {
-                add(std::stod(token));
-            }
-            else
-            {
-                double b = pop();
-                double a = pop();
-                switch (token[0])
-                {
-                    case '+':
-                        add(a + b);
-                        break;
-                    case '-':
-                        add(a - b);
-                        break;
-                    case '*':
-                        add(a * b);
-                        break;
-                    case '/':
-                        add(a / b);
-                        break;
-                    default:
-                        std::cerr<<"Error: the program doesn't work with " << token << std::endl;
-                        break;
-                }
-            }
-        }
-        *result=pop();
-        delete[] mstack;
-    }
-};
+#include "Calc.cpp"
 
 int main() {
     std::ifstream source;
---
+++ Assignment2/b/Calculator.h
@@ -0,0 +1,9 @@
+/* Ronshin Vsevolod st132572@student.spbu.ru
+    first project
+*/
+
+#ifndef CACLULATOR.h
+#define CACLULATOR.h
+#include <string>
+class Calc;
+#endif
