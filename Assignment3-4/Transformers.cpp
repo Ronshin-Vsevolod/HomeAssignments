6198d193d2b6bd2a17c97b87d83c114b94f4a22c
Ronshin-Vsevolod <st132572@student.spbu.ru>
2024-11-02 10:32:49 +0300

    Removed unnecessary commented code

--- Assignment3-4/Transformers.cpp
+++ Assignment3-4/Transformers.cpp
@@ -10,47 +10,6 @@ Work to explore class hierarchy and google test.Creating characteristics of tran
 #include "Decepticon.h"
 #include "Dinobot.h"
 
-/*
-std::ostream & operator <<(std::ostream & os, Transformer const & transformer)
-{
-    os <<"Information: " << std::endl << "Transformer" << std::endl;
-    os << transformer.get_speed() << std::endl;
-    os << transformer.get_eyes() << std::endl;
-    os << transformer.get_guntype() << std::endl;
-    os << transformer.get_ammo() << std::endl;
-    return os;
-}
-
-std::ostream & operator <<(std::ostream & os, Autobot const & autobot)
-{
-    os <<"Information: " << std::endl << "Autobot" << std::endl;
-    os << autobot.get_speed() << std::endl;
-    os << autobot.get_eyes() << std::endl;
-    os << autobot.get_guntype() << std::endl;
-    os << autobot.get_ammo() << std::endl;
-    return os;
-}
-
-std::ostream & operator <<(std::ostream & os, Decepticon const & decepticon)
-{
-    os <<"Information: " << std::endl << "Decepticon" << std::endl;
-    os << decepticon.get_speed() << std::endl;
-    os << decepticon.get_eyes() << std::endl;
-    os << decepticon.get_guntype() << std::endl;
-    os << decepticon.get_ammo() << std::endl;
-    return os;
-}
-
-std::ostream & operator <<(std::ostream & os, Dinobot const & dinobot)
-{
-    os <<"Information: " << std::endl << "Dinobot" << std::endl;
-    os << dinobot.get_speed() << std::endl;
-    os << dinobot.get_eyes() << std::endl;
-    os << dinobot.get_guntype() << std::endl;
-    os << dinobot.get_ammo() << std::endl;
-    return os;
-}
-*/
 int main()
 {
     std::cout<<"The main program has started and is running successfully"<<std::endl;
